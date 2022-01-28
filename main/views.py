e15f6ed2d0a4b16d908c1ed7834b3066b8dcc09e
udokaamos <udokaamos@gmail.com>
2022-01-28 20:04:33 +0100

    First API, and first push

---
+++ main/views.py
@@ -0,0 +1,24 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .serializers import UserSerializer
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+@api_view(['GET', 'POST'])
+def user_view(request):
+
+    if request.method == 'GET':
+        all_users = User.objects.all()
+
+
+        serializer = UserSerializer(all_users, many=True)
+
+        data = {
+            "message":"successful",
+            "data": serializer.data
+        }
+
+        return Response(data, status=status.HTTP_200_OK)
